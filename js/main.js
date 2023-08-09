3fef6d24b3008acceec930f9ffe7601f0cd53547
Daff <dafiulhaq97@gmail.com>
2023-08-09 23:24:56 +0700

    feat: add backend integration

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const BACKEND_HOST = 'https://ragakita-backend.vercel.app'
+
+const waitlistFormEl = document.getElementById('email-row-emailForm')
+const requestResultEl = document.getElementById('requestResult')
+
+let isSubmitSuccess = false
+let lastSubmitTime = 0
+
+waitlistFormEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nowTime = Date.now();
+    if (isSubmitSuccess || (nowTime < lastSubmitTime + 2500)) return;
+    lastSubmitTime = nowTime;
+
+    const email = waitlistFormEl.elements.email.value;
+    fetch(BACKEND_HOST + '/api/waitlist', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email })
+    }).then((res) => {
+        return res.json();
+    }).then((data) => {
+        if (data.success) {
+            requestResultEl.textContent = 'Emailmu berhasil ditambahkan ke waitlist'
+            isSubmitSuccess = true
+        } else {
+            requestResultEl.textContent = 'Gagal menambahkan email ke waitlist'
+            console.error(data.error)
+        }
+    }).catch((err) => {
+        requestResultEl.textContent = 'Unexpected error'
+        console.error(err)
+    })
+});
